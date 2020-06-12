2337dcdb4e4ae46e9a29858c41ee68e0d4658a69
XHS-12302 <xhsgg12302@gmail.com>
2020-06-12 20:14:27 +0800

    SelectSort demo

--- src/main/java/_algorithm/sort/SelectSort.java
+++ src/main/java/_algorithm/sort/SelectSort.java
@@ -4,14 +4,16 @@ import org.junit.Test;
 
 import java.util.Arrays;
 
-public class BubbleSort {
+public class SelectSort {
 
-    public void bs(int[] raw){
-        for (int i = 1; i < raw.length ; i++) {
-            for (int j = 0; j < raw.length - i ; j++) {
-                if(raw[j] > raw[j+1])
-                    SortUtils.switchNumberByTemp(raw,j,j+1);
+    public static void ss(int[] raw){
+        for (int i = 0; i < raw.length - 1 ; i++) {
+            int temp = i;
+            for (int j = i + 1; j < raw.length ; j++) {
+                if(raw[temp] > raw[j])
+                    temp = j;
             }
+            SortUtils.switchNumberByXor(raw,i,temp);
         }
     }
 
@@ -19,7 +21,7 @@ public class BubbleSort {
     public void test(){
         int[] generator = SortUtils.generatorByMathRandom(10, 0, 100);
         System.out.println(Arrays.toString(generator));
-        bs(generator);
+        ss(generator);
         System.out.println(Arrays.toString(generator));
 
     }
@@ -27,6 +29,6 @@ public class BubbleSort {
     @Test
     public void performance(){
         int[] generator = SortUtils.generatorByMathRandom(80000, 0, 800000000);
-        System.out.println(SortUtils.performanceByStopWatch((ints)-> bs(generator)) + "ms");
+        System.out.println(SortUtils.performanceByInstant((ints)-> ss(generator)) + "ms");
     }
 }
