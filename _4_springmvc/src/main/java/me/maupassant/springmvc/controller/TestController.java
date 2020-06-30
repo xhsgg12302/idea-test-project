0aa5e4d957b8bd4d58fb8dab286008bf17acadf0
XHS-12302 <xhsgg12302@gmail.com>
2020-07-01 02:01:16 +0800

    algorithm part of permutation and combination implement , other module consummate

--- _4_springmvc/src/main/java/me/maupassant/springmvc/controller/TestController.java
+++ _4_springmvc/src/main/java/me/maupassant/springmvc/controller/TestController.java
@@ -11,7 +11,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Date;
+import java.util.UUID;
 
 /**
  * Copyright 2018 ...com Inc. All Rights Reserved.
@@ -69,5 +73,40 @@ public class TestController {
         User user = new User("909","world","hello");
         Integer result = iTest.testTransaction(user);
         return result;
+    }
+
+
+    /**
+     * #测试 session跨域（一种session共享解决方案）
+     *
+     * 1,设置的domain （wtfu.site) 在控制台自动变化为（.wtfu.site）
+     * 2,通过两个域名{
+     *          http://www.wtfu.site:8080/test/cookie.do,
+     *          http://sso.wtfu.site:8080/test/cookie.do
+     *      }访问，浏览器确实可以将wtfu的cookie信息附入sso请求中。
+     *
+     * @param request
+     * @param response
+     * @return
+     */
+    @RequestMapping(value = "/cookie")
+    @ResponseBody
+    public String testCookie(HttpServletRequest request,HttpServletResponse response){
+        boolean flag = false;
+        Cookie[] cookies = request.getCookies();
+        if(cookies != null){
+            for (Cookie cookie : cookies) {
+                if(cookie.getName().equals("custom_global_session_id")){
+                    flag = true;
+                }
+            }
+        }
+        if(!flag){
+            Cookie cookie = new Cookie("custom_global_session_id", UUID.randomUUID().toString());
+            cookie.setPath("/");
+            cookie.setDomain("wtfu.site");
+            response.addCookie(cookie);
+        }
+        return "Test Ok";
     }
 }
