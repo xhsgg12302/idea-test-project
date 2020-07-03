59cd4765b5a471aa1ec06d38948f6a659429e5c2
12302 <xhsgg12302@126.com>
2020-07-04 06:18:11 +0800

    SAX ，SAX2 parse 小毛驴，and tomcat digester analyst

---
+++ src/main/java/_base/xml_parse/digester/entity/Class.java
@@ -0,0 +1,22 @@
+package _base.xml_parse.digester.entity;
+
+public class Class {
+    private String name;
+    private int number;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
