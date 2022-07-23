027a73650228b6d172873f1e571421a164d41210
XHS-12302 <xhsgg12302@gmail.com>
2022-07-23 21:51:18 +0800

    _4_ spring mvc @EnableWebMvc add swagger-ui module and test successfully

--- _0_base-learning/src/main/java/_base/thread/_synchronized/Demo.java
+++ _0_base-learning/src/main/java/_base/thread/_synchronized/Demo.java
@@ -4,6 +4,9 @@ package _base.thread._synchronized;
  *
  * 加synchronized的方法，线程会读锁，
  * 而没有加的线程不会读锁，直接进入执行
+ *
+ * 20220722 add， synchronized 在方法上锁住的是对象。即使同一个对戏那个不同syn方法，也会出现同步的情况。
+ *                对象的锁和类的锁不冲突（也就是不同步）
  */
 public class Demo {
 
@@ -11,10 +14,27 @@ public class Demo {
         System.out.println(Thread.currentThread().getName() + " m1 start....");
         try {
             Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        } catch (InterruptedException e) {e.printStackTrace();}
         System.out.println(Thread.currentThread().getName() + " m1 end");
+    }
+
+    /**
+     * 测试两个线程同时进入synchronized 方法
+     */
+    public static synchronized void m11(){
+        System.out.println(Thread.currentThread().getName() + " m11 start....");
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {e.printStackTrace();}
+        System.out.println(Thread.currentThread().getName() + " m11 end");
+    }
+
+    public void normal(){
+        System.out.println(Thread.currentThread().getName() + " normal start....");
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {e.printStackTrace();}
+        System.out.println(Thread.currentThread().getName() + " normal end");
     }
 
     public  void m2(){
@@ -66,8 +86,14 @@ public class Demo {
 
     public static void main(String args[]){
         Demo demo = new Demo();
-        new Thread(demo::m4).start();
-        new Thread(demo::m2).start();
+        //new Thread(demo::m4).start();
+        //new Thread(demo::m2).start();
 
+        new Thread(demo::m1).start();
+        new Thread(Demo::m11).start();
+        new Thread(demo::normal).start();
     }
+
+
+
 }
