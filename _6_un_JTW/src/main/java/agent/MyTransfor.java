38bc8bb1c3a1523fd5f30f77c47e6a385cade7a0
XHS-12302 <xhsgg12302@gmail.com>
2023-12-26 04:00:58 +0800

    feat: update _6_un_JTW

--- _6_un_JTW/src/main/java/agent/MyTransfor.java
+++ _6_un_JTW/src/main/java/agent/MyTransfor.java
@@ -1,4 +1,4 @@
-package demo;
+package agent;
 
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
@@ -16,7 +16,7 @@ public class MyTransfor implements ClassFileTransformer {
     public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
 
         /*_12302_2019-07-26_<此处可以静态代理>*/
-        System.out.println("hello agent!");
+        System.out.println("from: " + className + "--> hello agent!");
 
 
         /*_12302_2019-07-26_<return null 并不表示将要加载的类清空,而是不往其中添加任何东西>*/
