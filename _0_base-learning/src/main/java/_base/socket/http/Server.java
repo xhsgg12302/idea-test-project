513a2ff544741d4d3e8e21c3144b520b70373971
XHS-12302 <xhsgg12302@gmail.com>
2024-02-02 22:44:06 +0800

    refactor: add refer for Server.java

--- _0_base-learning/src/main/java/_base/socket/http/Server.java
+++ _0_base-learning/src/main/java/_base/socket/http/Server.java
@@ -15,6 +15,10 @@ import java.nio.charset.StandardCharsets;
  * 
  */
 public class Server {
+    /**
+     * see: http://localhost:3000/#/docs/doc/base/network?id=http-impl
+     * @param args
+     */
     public static void main(String[] args) {
         // You can use print statements as follows for debugging, they'll be visible when running tests.
         System.out.println("Logs from your program will appear here!");
