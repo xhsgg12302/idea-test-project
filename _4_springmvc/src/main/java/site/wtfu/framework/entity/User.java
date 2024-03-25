ce44da3af4564b6e55b15817ecc9d881f74e2d2a
XHS-12302 <xhsgg12302@gmail.com>
2024-03-26 07:54:21 +0800

    refactor: update mvc struct 06

--- _4_springmvc/src/main/java/site/wtfu/framework/entity/User.java
+++ _4_springmvc/src/main/java/site/wtfu/framework/entity/User.java
@@ -9,72 +9,35 @@
  */
 package site.wtfu.framework.entity;
 
-import java.util.Date;
+import lombok.Data;
 
+import java.time.LocalDateTime;
 
 /**
  * @className User
  * @description
  *
  */
+@Data
 public class User {
-	private String name;
-	private Integer age;
-	private String account;
-	private String password;
-	private Date time;
 
-	public User() {
-	}
+	private String name;
+
+	private Integer age;
+
+	private String account;
+
+	private String password;
+
+	private LocalDateTime time;
+
+	private LocalDateTime now;
+
+	public User() {}
 
 	public User(String name, String account, String password) {
 		this.name = name;
 		this.account = account;
 		this.password = password;
-	}
-
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public Integer getAge() {
-		return age;
-	}
-
-	public void setAge(Integer age) {
-		this.age = age;
-	}
-
-	public String getAccount() {
-		return account;
-	}
-	public void setAccount(String account) {
-		this.account = account;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public Date getTime() {
-		return time;
-	}
-	public void setTime(Date time) {
-		this.time = time;
-	}
-
-	@Override
-	public String toString() {
-		return "User{" +
-				"name='" + name + '\'' +
-				", age=" + age +
-				", account='" + account + '\'' +
-				", password='" + password + '\'' +
-				", time=" + time +
-				'}';
 	}
 }
