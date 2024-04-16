6603d58720ed2a04b7931913a54bacc978674a3c
XHS-12302 <xhsgg12302@gmail.com>
2024-04-16 12:23:58 +0800

    feature: add aop:config to spring framework test unit

--- _0_base-learning/src/main/java/_framework/spring/bean_lifecycle/SpringBeanLifeCycleTest.java
+++ _0_base-learning/src/main/java/_framework/spring/bean_lifecycle/SpringBeanLifeCycleTest.java
@@ -10,9 +10,9 @@ public class SpringBeanLifeCycleTest {
     public static void main(String[] args) {
 
         // 获取xml文件的一种方式。
-        URL config = SpringBeanLifeCycleTest.class.getResource("../res/Bean-LifeCycle.xml");
+        URL config = SpringBeanLifeCycleTest.class.getResource("../res/bean-lifecycle.xml");
 
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("_framework/spring/res/Bean-LifeCycle.xml");
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("_framework/spring/res/bean-lifecycle.xml");
         Book book = (Book)applicationContext.getBean("book");
         System.out.println("Book name is " + book.getBookName());
         ((ClassPathXmlApplicationContext) applicationContext).destroy();
